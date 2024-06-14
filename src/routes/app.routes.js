ea3ae2b96e573632acc7d7725ef0c065712eb41b
mateus.cabral <mateuscabral7575@gmail.com>
2024-06-14 00:05:22 -0300

    Testing

--- src/routes/app.routes.js
+++ src/routes/app.routes.js
@@ -7,6 +7,7 @@ import PatientDetails from "../components/PatientDetails";
 import Students from "../components/Students";
 import StudentDetails from "../components/StudentDetails";
 import Patient from "../components/Patient";
+import Agendamentos from "../components/Agendamentos";
 
 const Drawer = createDrawerNavigator();
 
@@ -31,13 +32,14 @@ export default function AppRoutes() {
           headerTitle: "",
         }}
       />
+      <Drawer.Screen name="Todos os Agendamentos" component={Agendamentos} />
       <Drawer.Screen name="Cadastrar Fisioterapeuta" component={CreateFisio} />
       <Drawer.Screen name="Gerenciar Alunos" component={Students} />
       {/* Detalhes do paciente e prontuario será uma stack navigation */}
+      <Drawer.Screen name="Detalhes do Aluno" component={StudentDetails} />
+      <Drawer.Screen name="Pacientes" component={Patient} />
       <Drawer.Screen name="Detalhes do Paciente" component={PatientDetails} />
       <Drawer.Screen name="Prontuario" component={Prontuario} />
-      <Drawer.Screen name="Detalhes do estudante" component={StudentDetails} />
-      <Drawer.Screen name="Pacientes" component={Patient} />
     </Drawer.Navigator>
   );
 }
